8e77a38f6360df14ece424e7d9eb447768096554
ZARGHAM AHMED KHAN <zarghamahmedkhan@gmail.com>
2021-06-29 17:57:07 +0530

    Pattern Programming  in java
    Pattern Programming  in java

---
+++ Pattern9.java
@@ -0,0 +1,18 @@
+package com.patternprogramming;
+
+public class Pattern9 {
+
+	public static void main(String[] args)
+	{
+		for(int i=1;i<=5;i++)
+		{
+			for(int k=1;k<=i;k++)
+			{
+				System.out.print(k);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
